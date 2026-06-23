ed1ce98377b99fc47b707dd10b7aa27f12e02755
libinghui <libinghui@zhanqi.com>
2026-06-23 15:33:13 +0800

    feat: 分享优化

--- src/app.ts
+++ src/app.ts
@@ -50,30 +50,4 @@ class App extends Component<AppProps> {
   }
 }
 
-export default App
-import Taro from '@tarojs/taro'
-import { Component } from 'react'
-import './app.css'
-
-class App extends Component {
-  componentDidMount() {
-    // 初始化云开发
-    if (process.env.TARO_ENV === 'weapp') {
-      Taro.cloud.init({
-        env: process.env.TARO_APP_CLOUD_ENV_ID || '',
-        traceUser: true,
-      })
-      console.log('云开发初始化完成')
-    }
-  }
-
-  componentDidShow() {}
-
-  componentDidHide() {}
-
-  render() {
-    return this.props.children
-  }
-}
-
-export default App
+export default App
